e7f37707bbe76273f9a82df342a9d1d82e819a48
branko <50580893+branko4@users.noreply.github.com>
2022-07-21 13:04:49 +0200

    Setup default projects 2 frontends and 2 backends

---
+++ Backend/M2MT-POC-002.BackendTwo/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace M2MT_POC_002.BackendTwo
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
